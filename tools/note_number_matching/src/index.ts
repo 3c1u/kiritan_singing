d943ee3b4a9d2177cb6f134dbcdba7e3ce303472
Hikaru Terazono (3c1u) <3c1u@tohkani.com>
2021-05-10 10:14:52 +0900

    DP matching (terrible quality)

--- tools/note_number_matching/src/index.ts
+++ tools/note_number_matching/src/index.ts
@@ -7,6 +7,7 @@ interface Label {
   startTime: number
   lyrics: string
   noteNumber?: number
+  exactMatch?: boolean
 }
 
 const parseMonoLabels = (labels: string): Label[] => {
@@ -75,42 +76,70 @@ const matchLabels = async (id: number, prefix: string = datasetPrefix) => {
     }
   }
 
-  // for debug:
-  for (let i = 0; i <= nMono; ++i) {
-    const p = nMono - i
-    const ly = p == 0 ? '' : monoLabel[p - 1].lyrics
-    process.stdout.write(ly.padStart(4))
-
-    process.stdout.write(dp[p].map(v => `${v}`.padStart(4)).join(' '))
-    process.stdout.write('\n')
-  }
-
-  process.stdout.write('\n         ')
+  process.stdout.write('\n             ')
   process.stdout.write(monoNoteLabel.map(v => v.lyrics.padStart(4)).join(' '))
   process.stdout.write('\n')
 
   let y_note = nNote
   let x_mono = nMono
 
-  while (y_note != 0) {
+  while (y_note !== 0 && x_mono !== 0) {
     const dLeft = dp[x_mono - 1][y_note]
     const dBottom = dp[x_mono][y_note - 1]
     const dLeftBottom = dp[x_mono - 1][y_note - 1]
 
     if (dLeft < dBottom && dLeft < dLeftBottom) {
+      if (monoLabel[x_mono - 1].noteNumber === undefined) {
+        const label = monoLabel[x_mono - 1]
+        const labelWithNote = monoNoteLabel[y_note - 1]
+  
+        label.noteNumber = labelWithNote.noteNumber
+        label.exactMatch = labelWithNote.lyrics === label.lyrics
+      }
+
       x_mono--
       continue
     }
 
     if (dBottom < dLeftBottom) {
+      if (!monoLabel[x_mono - 1].exactMatch) {
+        const label = monoLabel[x_mono - 1]
+        const labelWithNote = monoNoteLabel[y_note - 1]
+  
+        label.noteNumber = labelWithNote.noteNumber
+        label.exactMatch = labelWithNote.lyrics === label.lyrics
+      }
+
       y_note--
+
+      if (y_note === 0) {
+        break
+      }
+
+      if (!monoLabel[x_mono - 1].exactMatch) {
+        const label = monoLabel[x_mono - 1]
+        const labelWithNote = monoNoteLabel[y_note - 1]
+  
+        label.noteNumber = labelWithNote.noteNumber
+        label.exactMatch = labelWithNote.lyrics === label.lyrics
+      }
+
       continue
+    }
+
+    if (!monoLabel[x_mono - 1].exactMatch) {
+      const label = monoLabel[x_mono - 1]
+      const labelWithNote = monoNoteLabel[y_note - 1]
+
+      label.noteNumber = labelWithNote.noteNumber
+      label.exactMatch = labelWithNote.lyrics === label.lyrics
     }
 
     x_mono--
     y_note--
   }
 
+  // fill unknown note with zeros
   monoLabel.forEach(v => {
     if (nonVoicedLabel.includes(v.lyrics)) {
       v.noteNumber = 0
@@ -119,6 +148,24 @@ const matchLabels = async (id: number, prefix: string = datasetPrefix) => {
 
     v.noteNumber = v.noteNumber ?? 0
   })
+
+  // for debug:
+  for (let i = 0; i <= nMono; ++i) {
+    const p = nMono - i
+    const ly = p == 0 ? '' : monoLabel[p - 1].lyrics
+    const nn = p == 0 ? '' : monoLabel[p - 1].noteNumber ?? '?'
+    process.stdout.write(ly.padStart(4))
+    process.stdout.write(`${nn}`.padStart(4))
+
+    if (p) {
+      process.stdout.write(dp[p].map(v => `${v}`.padStart(4)).join(' '))
+    } else {
+      process.stdout.write('     ')
+      process.stdout.write(monoNoteLabel.map(v => `${v.noteNumber}`.padStart(4)).join(' '))
+    }
+  
+    process.stdout.write('\n')
+  }
 }
 
 const main = async () => {
